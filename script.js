3b041fbe1f005b2578fce4c19ad9d36c70ca4508
intellectualcoder <sanaq1992@live.com>
2024-09-05 00:03:46 -0500

    touchups

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     alert('Artist says: "Welcome to my Art page!"');
 });
+
+document.getElementById('burger-menu').addEventListener('click', function() {
+    var menu = document.getElementById('nav-menu');
+    menu.classList.toggle('active');
+});
