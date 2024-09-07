d4ae31f1e2a4fc7ab698537985b947f33b6fe917
intellectualcoder <sanaq1992@live.com>
2024-09-07 16:37:37 -0500

    added glow to header and a website counter

--- script.js
+++ script.js
@@ -21,3 +21,27 @@ document.getElementById('burger-menu').addEventListener('click', function() {
         });
     });
 </script>
+
+// Visitor Counter
+let visitCount = localStorage.getItem('page_view');
+
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem('page_view', visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem('page_view', 1);
+}
+
+document.getElementById('count').innerText = visitCount;
+
+// Fetch Visitor Location
+fetch('https://ipinfo.io/json?token=YOUR_TOKEN') // Get your token from ipinfo.io
+    .then(response => response.json())
+    .then(data => {
+        const locationInfo = `${data.city}, ${data.region}, ${data.country}`;
+        document.getElementById('location').innerText = `Your Location: ${locationInfo}`;
+    })
+    .catch(error => {
+        document.getElementById('location').innerText = 'Unable to detect location.';
+    });
